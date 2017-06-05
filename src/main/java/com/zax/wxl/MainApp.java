40ce8f8d02f189ed38cf47eeb85825a07a62cb85
zaheer <zaheer@zaheer.com>
2017-06-05 14:24:22 +0200

    First

---
+++ src/main/java/com/zax/wxl/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.zax.wxl;
+
+/**
+ * @author zaheer
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
