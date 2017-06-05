1517592d87ccf7ccfe3c1dea003248dca1d01bce
zaheer <zaheer@zaheer-laptop.fritz.box>
2017-06-05 15:10:31 +0200

    gui buttons added

--- src/main/java/com/zax/wxl/MainApp.java
+++ src/main/java/com/zax/wxl/MainApp.java
@@ -3,6 +3,12 @@
  */
 package com.zax.wxl;
 
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+
+import com.zax.wxl.gui.MainGUI;
+
 /**
  * @author zaheer
  *
@@ -13,7 +19,17 @@ public class MainApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					MainGUI frame = new MainGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});	
 
 	}
 
